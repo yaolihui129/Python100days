fa2c731bbb4722b23eb74e7cde099499069adb13
yaolihui129 <yaolihui0506>
2019-12-19 16:50:22 +0800

    day01-test1

--- day01_15/day01/test1.py
+++ day01_15/day01/test1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import turtle
 
 turtle.pensize(4)
@@ -11,6 +10,3 @@ turtle.forward(100)
 turtle.right(90)
 turtle.forward(100)
 turtle.mainloop()
-=======
-print('121')
->>>>>>> origin/master
